240d38113d3051ef83bb5f1e27505e1201d2ca4c
atlas <djlim@rsquare.co.kr>
2023-09-19 14:52:24 +0900

    [add] navigation

--- Unit4/Pathway3/basic-android-kotlin-compose-training-reply-app-nav-update/app/src/main/java/com/example/reply/MainActivity.kt
+++ Unit4/Pathway3/basic-android-kotlin-compose-training-reply-app-nav-update/app/src/main/java/com/example/reply/MainActivity.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 The Android Open Source Project
+ * Copyright (C) 2023 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,31 +13,34 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.example.reply
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.reply.ui.ReplyApp
 import com.example.reply.ui.theme.ReplyTheme
 
-/**
- * Activity for Reply app
- */
 class MainActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             ReplyTheme {
                 Surface {
-                    ReplyApp(windowSize =  WindowWidthSizeClass.Medium)
+                    val windowSize = calculateWindowSizeClass(this)
+
+                    ReplyApp(
+                        windowSize = windowSize.widthSizeClass,
+                    )
                 }
             }
         }
@@ -46,11 +49,12 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
-fun ReplyAppPreview() {
+fun ReplyAppCompactPreview() {
     ReplyTheme {
-        Surface (color = MaterialTheme.colorScheme.primary){
-            ReplyApp(windowSize = WindowWidthSizeClass.Compact)
+        Surface {
+            ReplyApp(
+                windowSize = WindowWidthSizeClass.Compact,
+            )
         }
-
     }
 }
---
+++ Unit4/Pathway3/basic-android-kotlin-compose-training-reply-app/app/src/main/java/com/example/reply/ui/WindowStateUtils.kt
@@ -0,0 +1,7 @@
+package com.example.reply.ui
+
+
+enum class ReplyContentType {
+    LIST_ONLY, LIST_AND_DETAIL
+}
+
